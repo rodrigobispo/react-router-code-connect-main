0b0dbf162e4b12f65480392bd94c0e3298406bac
Rodrigo Augusto Carlos Bispo <rodrigo.bispo@infrasa.gov.br>
2025-12-17 10:39:24 -0300

    config: configura e aplica rotas declarativas.

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,17 @@ import { Login } from './pages/Login/index.jsx'
 import { Register } from './pages/Register/index.jsx'
 import { Feed } from './pages/Feed/index.jsx'
 import { BlogPost } from './pages/BlogPost/index.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
-    {/* <Register /> */}
-    {/* <Feed /> */}
-    {/* <BlogPost /> */}
+    <BrowserRouter>
+      <Routes>
+        <Route path='/auth/login' element={<Login />} />
+        <Route path='/auth/register' element={<Register />} />
+        <Route path='/' element={<Feed />} />
+        <Route path='/blog-post' element={<BlogPost />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
